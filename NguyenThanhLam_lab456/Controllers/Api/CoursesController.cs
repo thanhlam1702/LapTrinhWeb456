456698cde76b979fb013385b6967851ca3ca5d36
ThanhLam <Administrator@PC232>
2020-06-18 15:52:52 +0700

    Uncancel

--- NguyenThanhLam_lab456/Controllers/Api/CoursesController.cs
+++ NguyenThanhLam_lab456/Controllers/Api/CoursesController.cs
@@ -30,5 +30,18 @@ namespace NguyenThanhLam_lab456.Controllers.Api
 
             return Ok();
         }
+
+        //[HttpDelete]
+        //public IHttpActionResult UnCancel(int id)
+        //{
+        //    var userId = User.Identity.GetUserId();
+        //    var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+        //    if (!course.IsCanceled)
+        //        return NotFound();
+        //    course.IsCanceled = false;
+        //    _dbContext.SaveChanges();
+
+        //    return Ok();
+        //}
     }
 }
